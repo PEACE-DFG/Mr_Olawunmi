f747ba71181a22d427a531f981429633fa2a7efa
PEACE-DFG <awofesobipeace@gmail.com>
2023-12-14 00:06:51 +0100

    JS DATES AND MATH

---
+++ eightjsclass/index.js
@@ -0,0 +1,78 @@
+// JAVASCRIPT DATES
+
+const date = new Date();
+
+console.log(date);
+
+// fullyear
+
+console.log(date.getFullYear());
+
+// month
+console.log(date.getMonth());
+
+// date
+
+console.log(date.getDate());
+
+// hours
+console.log(date.getHours());
+
+// minutes
+
+console.log(date.getMinutes());
+
+// seconds
+
+console.log(date.getSeconds());
+
+// milliseconds
+
+console.log(date.getMilliseconds());
+
+// days 
+
+console.log(date.getDay());
+
+// JS MATH
+// NUMBER TO INTEGERS
+
+/*
+4 METHODS
+
+1. Math.round(x)-> returns x rounded to its nearest integer
+
+2. Math.ceil(x)-> returns x rounded to its integer
+
+3. Math.floor(x)-> returns x rounded down to its integer
+
+4. Math.trunc(x) -> return integer part of x
+
+5. Math.pow(x, y) -> return the value of x to the power of y
+
+6. Math.sqrt(x)-> returns the square root of x
+
+7. Math.abs(x) -> returns the absolute value of x
+
+8. Math.log(x) -> returns the logarithm of x
+
+9. Math.sin(x) -> returns the sin of x
+
+10. Math.cos(x) -> returns the cos of x
+
+*/
+
+console.log(Math.round(4.6))
+console.log(Math.round(4.4))
+console.log(Math.ceil(4.4))
+console.log(Math.floor(4.9))
+console.log(Math.trunc(4.9))
+console.log(Math.pow(8,2))
+console.log(Math.sqrt(64))
+console.log(Math.abs(-4.7))
+console.log(Math.log(1))
+console.log(Math.sin(90))
+console.log(Math.cos(0))
+
+
+
